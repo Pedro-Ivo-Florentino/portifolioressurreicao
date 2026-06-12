613a3e29f10312920be62b9c5b80196046b094d9
Pedro Ribas <picefas@gmail.com>
2026-06-12 02:36:01 -0300

    Corrige rolagem inicial da página

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+if ("scrollRestoration" in window.history) {
+  window.history.scrollRestoration = "manual";
+}
+
+window.addEventListener("pageshow", () => {
+  if (!window.location.hash) {
+    window.scrollTo(0, 0);
+  }
+});
+
 if (window.lucide) {
   window.lucide.createIcons({
     attrs: {
